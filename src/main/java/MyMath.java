22b1cd46319046d3060078187513094d97843c01
M4karovVladimir <666.shadow.999@mail.ru>
2023-09-01 11:00:52 +0300

    create const

---
+++ src/main/java/MyMath.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MyMath {
+    private static final  double P = 3.14;
+    private static final   double E = 2.71;
+    public double tg = 0;
+    public double sin = 0;
+    public  double cos = 0;
+
+}
